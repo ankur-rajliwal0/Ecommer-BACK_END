aac9b6677afa8227a3746d295b9ac9834585fd9c
ankur-rajliwal0 <ankurflp@gmail.com>
2025-04-01 10:38:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express  = require("express");
+const app = express();
+const cors  = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+//DataBase
+
+PORT =5500 || process.env.PORT
+//Server 
+app.listen(PORT , ()=>{
+    console.log(`app is listen on http://localhost:${PORT}`)
+})
